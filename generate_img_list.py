aff62a30f143d3bc4bf9071a4f3d46c6ee06c61c
zhenghe <zhenghe2009@gmail.com>
2017-04-06 10:45:23 +0800

    Update generate_img_list.py
    产生txt list文件和读取txt文件到列表

--- generate_img_list.py
+++ generate_img_list.py
@@ -11,3 +11,21 @@ with open("img_list.csv", "w", newline="") as datacsv:
                 f = os.path.join(path, filename)
                 csvwriter.writerow ([f])
 
+# with open('thefile.txt', 'w') as file_object:
+#     x = []
+#     g = os.walk(img_dir)
+#     for path,d,filelist in g:
+#         for filename in filelist:
+#             if filename.endswith('jpg'):
+#                 f = os.path.join(path, filename)
+#                 f = f + '\n'
+#                 x.append(f)
+#     print(x)
+#     file_object.writelines(x)
+# file_object.close()
+
+# with open('thefile.txt', 'r') as file_object:
+#     lines = file_object.readlines()
+#     for line in lines:
+#         filename = line.split('\n')
+#         print(filename[0])
